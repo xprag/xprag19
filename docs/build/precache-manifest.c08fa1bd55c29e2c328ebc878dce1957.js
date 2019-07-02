fa83dbc801dd524d16eaa06236f5ef8b6a7fc36a
apierro <antonio.pierro@gmail.com>
2019-07-03 00:10:38 +0200

    Added bdocs/uild directory

---
+++ docs/build/precache-manifest.c08fa1bd55c29e2c328ebc878dce1957.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efab4fa555343999c535741c49ac9f19",
+    "url": "./index.html"
+  },
+  {
+    "revision": "002d9d72a66bd657f105",
+    "url": "./static/css/2.619dee99.chunk.css"
+  },
+  {
+    "revision": "034700f91d6cf4c40c2b",
+    "url": "./static/css/main.5656ad3a.chunk.css"
+  },
+  {
+    "revision": "002d9d72a66bd657f105",
+    "url": "./static/js/2.3d971d7f.chunk.js"
+  },
+  {
+    "revision": "034700f91d6cf4c40c2b",
+    "url": "./static/js/main.6a151920.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "9bb1eb80bd1fcf5ad5af6b4a5fadc351",
+    "url": "./static/media/cagliari.9bb1eb80.jpg"
+  },
+  {
+    "revision": "32fd30e72b3eb6616b5e0db7a9f6964b",
+    "url": "./static/media/citadel-of-museums.32fd30e7.jpg"
+  },
+  {
+    "revision": "34547df22d74eca5c56c6010a4e530e0",
+    "url": "./static/media/fondazione-sardegna.34547df2.jpg"
+  },
+  {
+    "revision": "073a3bbfe3daed310ecabc3f8c527c28",
+    "url": "./static/media/norbury.073a3bbf.png"
+  },
+  {
+    "revision": "5a5ccc03f281f0c8ade511be8a88e5ee",
+    "url": "./static/media/pickering.5a5ccc03.png"
+  },
+  {
+    "revision": "fd430f0a39dee208618199f0946154b2",
+    "url": "./static/media/universita-cagliari.fd430f0a.jpg"
+  }
+]);
