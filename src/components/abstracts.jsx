8311506edf940063cfa57c83ac5f9fc6e907983b
apierro <antonio.pierro@gmail.com>
2019-07-22 00:29:33 +0200

    Added the abstract section

---
+++ src/components/abstracts.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Abstracts = () => (
+  <main className="container">
+    <h2 className="mb-4">Abstracts</h2>
+    <p>
+      Abstracts will be published in an OSF project at the following
+      address:&nbsp;
+      <a className="text-warning" href="https://osf.io/wjqc5/">
+        https://osf.io/wjqc5/
+      </a>
+    </p>
+    <p>
+      The OSF meeting repository will be published at the following address:
+      forthcoming Organization of Book of Abstracts and OSF meeting repository:
+      Luca Bischetti, Elisabetta Tonini (IUSS Pavia)
+    </p>
+  </main>
+);
+
+export default Abstracts;
